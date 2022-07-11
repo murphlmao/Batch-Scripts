5ef1c857c82314439ff67ab31ca3b678a353aa3e
mmalcolm <mmalcolm@pmsi.cc>
2022-07-11 08:37:09 -0400

    Organize

---
+++ Kill presistent startup bloat.bat/close software on logon.bat
@@ -0,0 +1,29 @@
+@echo off
+
+:: taskill /F /IM _ /T
+
+taskill /F /IM Everything.exe /T
+
+taskill /F /IM OfficeClickToRun.exe /T
+
+taskill /F /IM TeamViewer_Service.exe /T
+
+taskill /F /IM vmware-usbarbitrator64.exe /T
+
+taskill /F /IM vmware-authd.exe /T
+
+taskill /F /IM SupportAssistAgent.exe /T
+
+taskill /F /IM HPPrintScanDoctorService.exe /T
+
+taskill /F /IM DellSupportAssistRemedationService.exe /T
+
+taskill /F /IM wpscenter.exe /T
+
+taskill /F /IM YourPhone.exe /T
+
+taskill /F /IM SmartByteTelemetry.exe /T
+
+
+
+
