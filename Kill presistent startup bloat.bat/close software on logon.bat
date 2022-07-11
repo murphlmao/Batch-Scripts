65606aa0d2727753da9171795c5ddacf240e13e0
mmalcolm <mmalcolm@pmsi.cc>
2022-07-11 08:52:13 -0400

    typo

--- Kill presistent startup bloat.bat/close software on logon.bat
+++ Kill presistent startup bloat.bat/close software on logon.bat
@@ -1,28 +1,28 @@
 @echo off
 
-:: taskill /F /IM _ /T
+:: taskkill /F /IM _ /T
 
-taskill /F /IM Everything.exe /T
+taskkill /F /IM Everything.exe /T
 
-taskill /F /IM OfficeClickToRun.exe /T
+taskkill /F /IM OfficeClickToRun.exe /T
 
-taskill /F /IM TeamViewer_Service.exe /T
+taskkill /F /IM TeamViewer_Service.exe /T
 
-taskill /F /IM vmware-usbarbitrator64.exe /T
+taskkill /F /IM vmware-usbarbitrator64.exe /T
 
-taskill /F /IM vmware-authd.exe /T
+taskkill /F /IM vmware-authd.exe /T
 
-taskill /F /IM SupportAssistAgent.exe /T
+taskkill /F /IM SupportAssistAgent.exe /T
 
-taskill /F /IM HPPrintScanDoctorService.exe /T
+taskkill /F /IM HPPrintScanDoctorService.exe /T
 
-taskill /F /IM DellSupportAssistRemedationService.exe /T
+taskkill /F /IM DellSupportAssistRemedationService.exe /T
 
-taskill /F /IM wpscenter.exe /T
+taskkill /F /IM wpscenter.exe /T
 
-taskill /F /IM YourPhone.exe /T
+taskkill /F /IM YourPhone.exe /T
 
-taskill /F /IM SmartByteTelemetry.exe /T
+taskkill /F /IM SmartByteTelemetry.exe /T
 
 
 
